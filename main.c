4fe0a712aae320d872ee92815e387acd98eea285
satone <498435443@QQ.COM>
2025-04-24 11:46:39 +0800

    update

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 #include <windows.h>
 
 
-int start_num = 100;//初始资金
+int start_num;//初始资金
 int win_rate = 51;//胜率
 int loop_num = 1000;
+int debt = 10;//赌注
 
 void gamble();
 int payout(int wager);
@@ -26,28 +27,65 @@ int main()
 
 }
 
-void gamble()
+void gamble(int num)
 {
-    srand((unsigned)time(NULL));
-    int max_num = 0;//收入最大值
-    int round = 1;//轮次
-    for(; start_num>=0 && round < loop_num; round++)
+    int start_num_temp = start_num;
+    //模式选择
+    if(num==1)
     {
-        //以10元为赌注
-        if(payout(10)>0)
+        srand((unsigned)time(NULL));
+        int max_num = 0;//收入最大值
+        int round = 1;//轮次
+        for(; start_num>=10 && round < loop_num; round++)
         {
-            start_num += 10;
-            if(max_num < start_num) max_num = start_num;
-            printf("Round%d:本轮获胜,剩余资金%d\n",round,start_num);
+            //以10元为赌注
+            if(payout(10)>0)
+            {
+                start_num += 10;
+                if(max_num < start_num) max_num = start_num;
+                printf("Round%d:本轮获胜,剩余资金%d\n",round,start_num);
+            }
+            else
+            {
+                start_num -= 10;
+                printf("Round%d:本轮失败,剩余资金%d\n",round,start_num);
+            }
         }
-        else
-        {
-            start_num -= 10;
-            printf("Round%d:本轮失败,剩余资金%d\n",round,start_num);
-        }
+        printf("--------------------模拟结束--------------------\n");
+        if(start_num<10) printf("本金不足，无法继续下注\n");
+        else printf("已达到设定循环次数\n");
+        printf("最高记录为%d\n",max_num);
+        start_num=start_num_temp;
     }
-    printf("--------------------模拟结束--------------------\n");
-    printf("最高记录为%d\n",max_num);
+
+    else if(num==2)
+    {
+        srand((unsigned)time(NULL));
+        int max_num = 0;//收入最大值
+        int round = 1;//轮次
+        for(; start_num>= debt*2 && round < loop_num; round++)
+        {
+            //以设定的金额为赌注
+            if(payout(debt)>0)
+            {
+                start_num += debt;
+                if(max_num < start_num) max_num = start_num;
+                printf("Round%d:本轮获胜,下局赌注为10,剩余资金%d\n",round,start_num);
+                debt = 10;
+            }
+            else
+            {
+                start_num -= debt;
+                printf("Round%d:本轮失败,赌注上升至%d,剩余资金%d\n",round,debt*2,start_num);
+                debt *=2;
+            }
+        }
+        printf("--------------------模拟结束--------------------\n");
+        if(start_num< debt*2) printf("本金不足，无法继续下注\n");
+        else printf("已达到设定循环次数\n");
+        printf("最高记录为%d\n",max_num);
+        start_num=start_num_temp;
+    }
 }
 
 //每次赌博的收益
@@ -158,8 +196,7 @@ void menu_jump(int num)
             break;
         case 2:
             system("cls");
-            printf("Test mode");
-            Sleep(1000);
+            mode_menu2();
             return 0;
             break;
         case 3:
@@ -171,6 +208,7 @@ void menu_jump(int num)
 void mode_menu1()
 {
     int ch1=0;
+    start_num = 100;
     while(1)
     {
         printf("------------------------------\n");
@@ -179,7 +217,7 @@ void mode_menu1()
         printf("------------------------------\n");
         printf("1.初始资金:%d\n",start_num);
         printf("2.胜率:%d%%\n",win_rate);
-        printf("3.循环次数:%d\n",win_rate);
+        printf("3.循环次数:%d\n",loop_num);
         printf("------------------------------\n");
         printf("按下序号对应的数字键以修改数值\n按下Y键开始测试\n按下N键返回菜单\n");
         ch1=getch();
@@ -191,22 +229,83 @@ void mode_menu1()
             system("cls");
             printf("开始模拟\n");
             Sleep(1000);
-            gamble();
+            gamble(1);
             system("pause");
         }
-        //key 1
+        //key 1 2 3
         else if(ch1 == 49)
+        {
+            printf("------------------------------\n");
+            printf("修改初始资金为");
+            start_num = scan_num(start_num);
+        }
+        else if(ch1 == 50)
         {
             printf("------------------------------\n");
             printf("修改胜率为");
             win_rate = scan_num(win_rate);
         }
-        else if(ch1 == 50)
+        else if(ch1 == 51)
         {
             printf("------------------------------\n");
             printf("修改循环次数为");
             loop_num = scan_num(loop_num);
         }
+        else
+            printf("\n无效按键 \n");
+        Sleep(1000);
+        system("cls");
+    }
+}
+//模式2
+void mode_menu2()
+{
+    int ch1=0;
+    start_num = 1000;
+    while(1)
+    {
+        printf("------------------------------\n");
+        printf("模拟名称：倍投\n");
+        printf("模拟规则：最开始以10元为赌注，获胜得20元，失败得0元。每次失败后将下注金额翻倍。\n");
+        printf("------------------------------\n");
+        printf("1.初始资金:%d\n",start_num);
+        printf("2.胜率:%d%%\n",win_rate);
+        printf("3.循环次数:%d\n",loop_num);
+        printf("------------------------------\n");
+        printf("按下序号对应的数字键以修改数值\n按下Y键开始测试\n按下N键返回菜单\n");
+        ch1=getch();
+        //key N
+        if(ch1 == 78 || ch1 == 110) return 0;
+        //key Y
+        else if(ch1 == 89 || ch1 == 121)
+        {
+            system("cls");
+            printf("开始模拟\n");
+            Sleep(1000);
+            gamble(2);
+            system("pause");
+        }
+        //key 1 2 3
+        else if(ch1 == 49)
+        {
+            printf("------------------------------\n");
+            printf("修改初始资金为");
+            start_num = scan_num(start_num);
+        }
+        else if(ch1 == 50)
+        {
+            printf("------------------------------\n");
+            printf("修改胜率为");
+            win_rate = scan_num(win_rate);
+        }
+        else if(ch1 == 51)
+        {
+            printf("------------------------------\n");
+            printf("修改循环次数为");
+            loop_num = scan_num(loop_num);
+        }
+        else
+            printf("\n无效按键 \n");
         Sleep(1000);
         system("cls");
     }
